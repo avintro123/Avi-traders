c0cd649fe0fdde9bcea2d5ad36ab4042d76100e6
avintro123 <138115162+avintro123@users.noreply.github.com>
2023-07-01 12:27:09 +0000

    added dynamic data

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Web designer',
+    'location': 'Shimla, India',
+    'salary': 'Rs. 10,00,000',
+  },
+  {
+    'id': 2,
+    'title': 'Programmer',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 14,00,000',
+  },
+  {
+    'id': 3,
+    'title': 'Front/Backend developer',
+    'location': 'Chandigarh, India',
+    'salary': 'Rs. 9,00,000',
+  },
+  {
+    'id': 4,
+    'title': 'Graphics',
+    'location': 'Kolkata, India',
+    'salary': 'Rs. 19,00,000',
+  },
+]
+
 
 @app.route("/")
-def hello_world():
-  return "Helllo world"
+def hello_avitraders():
+  return render_template('home.html', jobs=JOBS)
 
 
-print(__name__)
-if __name__=="__main__":
+if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
